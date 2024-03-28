afaea3dce3485b9923c96df6a2f96302d8fa5b2e
Kevin Ibanez <kevin.ibanez.dev@gmail.com>
2024-03-28 11:39:29 -0500

    First commit, initialized spring boot app

---
+++ src/main/java/app/ibanez/ibanezkanbanbackend/IbanezKanbanBackendApplication.java
@@ -0,0 +1,13 @@
+package app.ibanez.ibanezkanbanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanezKanbanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanezKanbanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/ibanez/ibanezkanbanbackend/IbanezKanbanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ibanez.ibanezkanbanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanezKanbanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
